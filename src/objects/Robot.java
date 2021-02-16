55ead8ba9914cb6446f5c9ffd6e4be6253febb89
meryar <adrien.guedet@gmail.com>
2021-02-16 10:56:41 +0100

    [ADD] robot now handles moving forward command

--- src/objects/Robot.java
+++ src/objects/Robot.java
@@ -9,6 +9,7 @@ import java.util.Vector;
 
 import environment.Direction;
 import environment.Environment;
+import environment.Environment.Touch;
 import environment.Object;
 import environment._2DMap;
 import robot.Action;
@@ -21,6 +22,7 @@ public class Robot extends Object{
 	private Queue<Vector> trace;
 	private int sensors_number;
 	private Vector<Sensor> sensors;  
+	private Action pending;
 	
 	public Robot(_2DMap map_, Color color_, Environment.Touch touch_, String name_,
 			boolean visible_, Point position, double visionRange) {
@@ -64,6 +66,7 @@ public class Robot extends Object{
 	public int getSensorNb() {return sensors_number;}
 
 	public void prepareAction(Action command) {
+		pending = command;
 		
 		switch(command) {
 		case MOVE_FWD:
@@ -80,8 +83,42 @@ public class Robot extends Object{
 		case ROTATE_RIGHT:
 			break;
 		default:
-			break;
+			throw new IllegalStateException("Illegal command from agent: " + command);
 		}
 		
+	}
+
+	public Action getResults() {
+		
+		switch(pending) {
+		case MOVE_FWD:
+			Point newPos = getMap().getOrientedRelPos(getPosition(), getDirection(), new Point(0,-1));
+			if (getMap().isLegalPosition(newPos)) {
+				if (getMap().isHard(newPos)) {
+					return Action.BUMP;
+				} else if (getMap().isFood(newPos)) {
+					List<Object> targets = getMap().getTypedObjects(Environment.Touch.FOOD, newPos);
+					for (Object fish: targets) {
+						if (((Fish) fish).was_eaten_by(getName())) {
+							return ((Fish) fish).getAffordedOnDeath();
+						}
+					}
+					return Action.FIGHT;
+				} else {
+					getMap().moveObject(getName(), getPosition(), newPos);
+					return Action.MOVE_FWD;
+				}
+			} else {
+				throw new IllegalStateException("Robot " + getName() + " is trying to exit map!");
+			}
+		case ROTATE_LEFT:
+			break;
+		case ROTATE_RIGHT:
+			break;
+		default:
+			throw new IllegalStateException("Illegal command pending: " + pending);
+		}
+		
+		return null;
 	}
 }
