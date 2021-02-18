288abdb6f9aded4fa03149c2dc9e0fcc9b95e221
meryar <adrien.guedet@gmail.com>
2021-02-18 11:50:23 +0100

    [ADD] basic perceptron skeleton

---
+++ src/agent_developpemental/Neuron.java
@@ -0,0 +1,16 @@
+package agent_developpemental;
+
+import java.util.Vector;
+
+public class Neuron {
+
+	public Neuron(int i) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Float compute(Vector<Float> entry) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
--- src/agent_developpemental/Perceptron.java
+++ src/agent_developpemental/Perceptron.java
@@ -1,14 +1,32 @@
 package agent_developpemental;
 
-import robot.Action;
+import java.util.Vector;
 
 public class Perceptron {
 
-	public Perceptron(int nbColors) {
-		
-		for(Action output: Action.values()) {
-			
+	Vector<Neuron> neurons;
+	float bias;
+	
+	public Perceptron() {
+		neurons = new Vector<Neuron>();
+		bias = (float) 1;
+	}
+	
+	public Perceptron(int input_size, int output_size) {
+		this();
+		for (int i=0; i<output_size; i++) {
+			neurons.add(new Neuron(input_size + 1));
 		}
+	}
+	
+	public Vector<Float> compute(Vector<Float> entry){
+		Vector<Float> res = new Vector<Float>();
+		
+		for (Neuron neuron: neurons) {
+			res.add(neuron.compute(entry));
+		}
+		
+		return res;
 	}
 
 }
