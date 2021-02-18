9d3c7929f877b593bebe10212a6f5c8c81dbbe0b
meryar <adrien.guedet@gmail.com>
2021-02-18 15:25:19 +0100

    [ADD] neuron initialization

--- src/agent_developpemental/Neuron.java
+++ src/agent_developpemental/Neuron.java
@@ -4,8 +4,16 @@ import java.util.Vector;
 
 public class Neuron {
 
-	public Neuron(int i) {
-		// TODO Auto-generated constructor stub
+	
+	private int entries_number;
+	private Vector<Float> weights;
+	
+	public Neuron(int input_size) {
+		weights = new Vector<Float>();
+		entries_number = input_size;
+		for (int i=0; i<input_size; i++) {
+			weights.add((float)(Math.random() * 2 - 1));
+		}
 	}
 
 	public Float compute(Vector<Float> entry) {
