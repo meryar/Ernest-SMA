7ece1c5d990eb1b5eb80ca36c108d648e728aab4
meryar <adrien.guedet@gmail.com>
2021-02-23 11:41:04 +0100

    [ADD] agent is now able to select next Action according to enactability certitude and interaction utility

--- src/agents/AgentDeveloppemental.java
+++ src/agents/AgentDeveloppemental.java
@@ -1,5 +1,8 @@
 package agents;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.Vector;import agent_developpemental.Neuron;
 import agent_developpemental.Perceptron;
 import main.Main;
@@ -11,10 +14,20 @@ public class AgentDeveloppemental extends Agent{
 	
 	private static final float certitude_treshold = 0.8f;
 	
+	private Map<Action,Float> utilities;
+	
 	private Perceptron perceptron;
 
 	public AgentDeveloppemental(int input_size) {
 		perceptron = new Perceptron(input_size, input_size);
+		utilities = new HashMap<>();
+		utilities.put(Action.MOVE_FWD, 5f);
+		utilities.put(Action.BUMP, -10f);
+		utilities.put(Action.FIGHT, -20f);
+		utilities.put(Action.EAT, 50f);
+		utilities.put(Action.FEAST, 200f);
+		utilities.put(Action.ROTATE_LEFT, -3f);
+		utilities.put(Action.ROTATE_RIGHT, -3f);
 	}
 
 	@Override
@@ -25,12 +38,53 @@ public class AgentDeveloppemental extends Agent{
 	}
 
 	private Action decideAction(Vector<Float> resultsTMinus1) {
+		// get predictions of success from perceptron
 		
 		Vector<Float> predictions = perceptron.compute(resultsTMinus1);
+		//System.out.println(predictions);
+		Action choice;
 		
-		System.out.println(predictions);
+		// find the most uncertain prediction
+		float min = 1f;
+		for (float pred: predictions) {
+			if (Math.abs(pred) < min) {
+				min = Math.abs(pred);
+			}
+		}
 		
-		return Action.MOVE_FWD;
+		int nb_sensors = (int) Math.pow(2*Main.robot_vision_range + 1, 2);
+		// if one prediction is under the certitude threshold
+		if (min < certitude_treshold) {
+			int min_index = predictions.indexOf(min);
+			if (min_index < Action.values().length * Main.colors.length * nb_sensors) {
+				choice = Action.values()[(int) Math.floor(min_index / (Main.colors.length * nb_sensors))];
+			} else {
+				choice = Action.values()[min_index - Action.values().length * Main.colors.length * nb_sensors];
+			}
+		} else {
+			choice = mostUseful(predictions);
+		}
+		
+		return choice;
+	}
+
+	private Action mostUseful(Vector<Float> predictions) {
+		float max_utility = Float.MIN_VALUE;
+		int max_index = -1;
+
+		List<Float> to_consider = predictions.subList(Action.values().length * Main.colors.length * (int) Math.pow(2*Main.robot_vision_range + 1, 2), predictions.size());
+
+		for (int i=0; i<to_consider.size(); i++) {
+			if (to_consider.get(i) > certitude_treshold) {
+				if (utilities.get(Action.values()[i]) > max_utility) {
+					max_utility = utilities.get(Action.values()[i]);
+					max_index = i;
+				}
+			}
+		}
+		
+		assert (max_index >= 0 && max_index < Action.values().length): "error in best movement calculation";
+		return Action.values()[max_index];
 	}
 
 	@Override
