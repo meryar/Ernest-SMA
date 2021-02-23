e2034d1d66742aebefaebbf459b3c8831908a78d
meryar <adrien.guedet@gmail.com>
2021-02-23 15:06:49 +0100

    [TEST] increased agent learning rate

--- src/main/Main.java
+++ src/main/Main.java
@@ -13,8 +13,8 @@ public class Main {
 	// program arguments (change them here)
 	private static final String env_path 			= "ressources/maps/Board_16x16_1.txt";	// path to the file containing the environment layout
 	public static final int trace_size 				= 10;	// number of previous actions of the robot displayed
-	public static final int robot_vision_range 	= 5;	// distance at which the robot is able to see
-	public static final float learning_rate			= 0.001f; // learning rate of the agent's perceptron
+	public static final int robot_vision_range 		= 5;	// distance at which the robot is able to see
+	public static final float learning_rate			= 0.1f; // learning rate of the agent's perceptron
 	public static final Color[] colors              = {Environment.WALL1,
 													   Environment.FIELD_COLOR,
 													   Environment.FISH1,
