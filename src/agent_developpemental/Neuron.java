2fbcaeb6d63b952af0dcde416087dbfc143336cd
meryar <adrien.guedet@gmail.com>
2021-02-25 11:43:46 +0100

    [ADD] neuron is now storing minimum weight

--- src/agent_developpemental/Neuron.java
+++ src/agent_developpemental/Neuron.java
@@ -2,19 +2,22 @@ package agent_developpemental;
 
 import java.util.Vector;
 
+import robot.Action;
+
 public class Neuron {
 	
 	private int entries_number;
 	private Vector<Float> weights;
 	private float c; 				// learning rate
 	
-	public float max_abs_weight; 
+	public float max_abs_weight, min_weight; 
 	
 	public Neuron(int input_size, float learning_rate) {
 		weights = new Vector<Float>();
 		entries_number = input_size;
 		c = learning_rate;
-		max_abs_weight = 1;
+		max_abs_weight = 1f;
+		min_weight = -0.001f;
 		
 		for (int i=0; i<entries_number; i++) {
 			weights.add(0f);
@@ -32,9 +35,13 @@ public class Neuron {
 	}
 	
 	public float learn(Vector<Float> entry, float error) {
+		min_weight = -0.001f;
         for (int i = 0; i < weights.size(); i++) {
             weights.set(i, weights.get(i) + c * error * entry.get(i));
-            max_abs_weight = Math.max(Math.abs(weights.get(i)), max_abs_weight);
+            if (i < weights.size() - Action.values().length) {
+	            max_abs_weight = Math.max(Math.abs(Math.abs(weights.get(i))), max_abs_weight);
+	            min_weight = Math.min(weights.get(i), min_weight);
+            }
         }
         return max_abs_weight;
 	}
