84375c20b5a2d6e7cc30c1d209b0392dd8423fe6
meryar <adrien.guedet@gmail.com>
2021-03-01 15:25:52 +0100

    [FIX] perceptron view is now able to display all views

--- src/view/PerceptronPanel.java
+++ src/view/PerceptronPanel.java
@@ -2,21 +2,32 @@ package view;
 
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.Point;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 
+import javax.swing.ButtonGroup;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JPanel;
+import javax.swing.JRadioButtonMenuItem;
 
 import agent_developpemental.Neuron;
 import agent_developpemental.Perceptron;
 import main.Main;
 import robot.Action;
 
-public class PerceptronPanel extends JPanel{
+public class PerceptronPanel extends JPanel implements ActionListener, MouseListener{
 
 	private static final long serialVersionUID = 1L;
 
 	private int pan_height, pan_width, screen_x_offset, screen_y_offset;
 	private Perceptron perceptron;
 	private int nb_sensors;
+	private int selected;
+	private JComboBox selection;
 	
 	public PerceptronPanel(Perceptron perceptron_, int pan_height_, int pan_width_) {
 		pan_height = pan_height_;
@@ -25,6 +36,20 @@ public class PerceptronPanel extends JPanel{
 		screen_y_offset = (int) (pan_height / 2);
 		screen_x_offset = (int) (pan_width / Action.values().length);
 		nb_sensors = (int) Math.pow(Main.robot_vision_range*2 + 1, 2);
+		selected = 0;
+
+		selection = new JComboBox();
+		selection.addItem("Move forward");
+		selection.addItem("Bump");
+		selection.addItem("Fight");
+		selection.addItem("Eat");
+		selection.addItem("Feast");
+		selection.addItem("Rotate left");
+		selection.addItem("Rotate right");
+		
+		selection.addActionListener(this);
+		addMouseListener(this);
+		add(selection);
 	}
 
 	@Override
@@ -54,9 +79,9 @@ public class PerceptronPanel extends JPanel{
 				int y = (int) (small_y_offset * (Math.floor(s / (Main.robot_vision_range*2 + 1))));
 
 				if(neuron.getWeights().get(i * offset + 0 * nb_sensors + s) == neuron.max_weight) {System.err.println("OK");}
-				float weight0Pos = normalize(neuron.getWeights().get(i * offset + 3 * nb_sensors + s), neuron.max_weight);
-				float weight1Pos = normalize(neuron.getWeights().get(i * offset + 1 * nb_sensors + s), neuron.max_weight);
-				float weight2Pos = normalize(neuron.getWeights().get(i * offset + 2 * nb_sensors + s), neuron.max_weight);
+				float weight0Pos = normalize(neuron.getWeights().get(selected * offset + 0 * nb_sensors + s), neuron.max_weight);
+				float weight1Pos = normalize(neuron.getWeights().get(selected * offset + 1 * nb_sensors + s), neuron.max_weight);
+				float weight2Pos = normalize(neuron.getWeights().get(selected * offset + 2 * nb_sensors + s), neuron.max_weight);
 
 				System.out.println("s " + s + " pos: w0 " + weight0Pos + "   w1 " + weight1Pos + "   w2 " + weight2Pos);
 				
@@ -67,9 +92,9 @@ public class PerceptronPanel extends JPanel{
 						));
 				g.fillRect(x, y, small_x_offset, small_y_offset);
 				
-				float weight0Neg = reverseNormalize(neuron.getWeights().get(i * offset + 3 * nb_sensors + s), neuron.min_weight);
-				float weight1Neg = reverseNormalize(neuron.getWeights().get(i * offset + 1 * nb_sensors + s), neuron.min_weight);
-				float weight2Neg = reverseNormalize(neuron.getWeights().get(i * offset + 2 * nb_sensors + s), neuron.min_weight);
+				float weight0Neg = reverseNormalize(neuron.getWeights().get(selected * offset + 0 * nb_sensors + s), neuron.min_weight);
+				float weight1Neg = reverseNormalize(neuron.getWeights().get(selected * offset + 1 * nb_sensors + s), neuron.min_weight);
+				float weight2Neg = reverseNormalize(neuron.getWeights().get(selected * offset + 2 * nb_sensors + s), neuron.min_weight);
 				
 				System.out.println("s " + s + " neg: w0 " + weight0Neg + "   w1 " + weight1Neg + "   w2 " + weight2Neg);
 				
@@ -93,6 +118,43 @@ public class PerceptronPanel extends JPanel{
 	
 	private float reverseNormalize(Float n, float min) {
 		return Math.max(0, n/min);
+	}
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		selected = selection.getSelectedIndex();
+		repaint();
+		
+	}
+
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		Point location = new Point(e.getX(), e.getY());
+		System.out.println(location);
+		
+	}
+
+	@Override
+	public void mousePressed(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseExited(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
 	}
 	
 }
