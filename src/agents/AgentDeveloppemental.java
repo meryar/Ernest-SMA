1cd9ec59c97f8c69c3cc506a65f07a2016c1a223
meryar <adrien.guedet@gmail.com>
2021-04-16 17:09:03 +0200

    [ADD] agents are now able to plan their movements over multiple steps

--- src/agents/AgentDeveloppemental.java
+++ src/agents/AgentDeveloppemental.java
@@ -4,6 +4,8 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.net.Socket;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
@@ -12,15 +14,30 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
+import javax.xml.crypto.Data;
+
 import agent_developpemental.SelectiveNeuron;
 import main.Main;
 import robot.Action;
 
 public class AgentDeveloppemental extends Agent{
 
+	private class DataStruct{
+		Action action;
+		float certitude;
+		int utility;
+		
+		DataStruct(Action action_, float certitude_, int utility_){
+			action = action_;
+			certitude = certitude_;
+			utility = utility_;
+		}
+	}
+	
 	private static final float certitude_treshold = 0.6f;
 	private static final float relevancy_treshold = 0.05f;
 	private static final int Thread_nb = 24;
+	private static final int Research_depth = 5;
 
 	private Map<Action,Integer> utilities;
 	private SelectiveNeuron[] primaries, secondaries;
@@ -74,49 +91,20 @@ public class AgentDeveloppemental extends Agent{
 	private Action decideAction(float[] perception) {
 		//long start = System.currentTimeMillis();
 		lastPerception = perception;
-
+		
 		Action choice;
-		float[] predSec = new float[secondaries.length];
 
-		// get predictions of success.... but EVEN FASTER
-		ExecutorService ex = Executors.newFixedThreadPool(Thread_nb);
-		int block_size = predSec.length/Thread_nb;
-		for (int i = 0; i < Thread_nb-1; i++) {
-			final int istart = i * block_size;
-			final int iend = (i + 1) * block_size;
-			ex.execute(new Runnable() {
-				@Override
-				public void run() {
-					for (int j=istart; j<iend; j++) {
-						predSec[j] = secondaries[j].compute(perception);
-					}
-				}
-			});
-		}
-		ex.execute(new Runnable() {
-			@Override
-			public void run() {
-				for (int j=(Thread_nb-1)*block_size; j<secondaries.length; j++) {
-					predSec[j] = secondaries[j].compute(perception);
-				}
-			}
-		});
-
-		ex.shutdown();
-		try {
-			ex.awaitTermination(1, TimeUnit.MINUTES);
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
-
-		// get predictions of success
+		// 1) get primary and secondary predictions
 		Vector<Float> predPrim = new Vector<Float>(primaries.length);
 		for (SelectiveNeuron neuron: primaries) {
 			predPrim.add(neuron.compute(perception));
 		}
+		float[] predSec = getPredSec(perception);
 		System.out.println(predPrim);
-
-		// isolate predictions for primary interactions
+		
+		// 2) check if there is something interesting to explore
+		
+		// 2.1) isolate enactable interactions
 		HashMap<Integer, Float> enactable = new HashMap<Integer, Float>();
 		for (int index=0; index<predPrim.size(); index++) {			
 			enactable.put(index + secondaries.length, predPrim.get(index));
@@ -128,7 +116,7 @@ public class AgentDeveloppemental extends Agent{
 			}
 		}
 
-		// find the most uncertain prediction
+		// 2.2) find the most uncertain prediction
 		float min_abs = 1f;
 		int min_index = -1;
 		for (int key: enactable.keySet()) {
@@ -137,21 +125,38 @@ public class AgentDeveloppemental extends Agent{
 				min_index = key;
 			}
 		}
-
+		
+		// 2.3) check if there is an uncertain prediction
 		if (min_abs < certitude_treshold) {
-			// if uncertain interaction then explore it
+			// 2.3.1) if uncertain interaction then explore it
 			System.out.println("exploring interaction " + min_index + " of absolute certitude " + min_abs);
 			if (min_index < secondaries.length) {
 				choice = Action.values()[(int)(min_index / (secondaries.length / Action.values().length))];
 			} else {
 				choice = Action.values()[min_index - secondaries.length];
 			}
-
 		} else {
-			System.out.println("agent in exploitation mode");
-			choice = mostUseful(predPrim);
+		// 3) else check if there is something interesting to explore after each enactable primary
+			// 3.1) get the least certain or most valuable interaction on each possible path
+			short nb_enctable_act = 0; 
+			Vector<Action> enactable_act = new Vector<Action>(); 
+			for (int i=0; i<predPrim.size(); i++) if (predPrim.get(i) >= certitude_treshold) {
+				nb_enctable_act += 1;
+				enactable_act.add(Action.values()[i]);
+			}
+			DataStruct[] choice_list = new DataStruct[nb_enctable_act];
+			for (int i=0; i<nb_enctable_act; i++) {
+				choice_list[i] = getValue(enactable_act.get(i), 
+						propagate(perception, enactable_act.get(i), predSec), 
+						0, 
+						utilities.get(enactable_act.get(i)));
+			}
+			
+			// 3.2) choose best action 
+			choice = choose_function(choice_list).action;
 		} 
-
+		
+		// 4) storing predictions
 		float[] buffer = new float[predSec.length + predPrim.size()];
 		for (int i=0; i<predSec.length; i++) {
 			buffer[i] = predSec[i];
@@ -162,8 +167,148 @@ public class AgentDeveloppemental extends Agent{
 		lastPrediction = buffer;
 		//System.out.println("deciding took " + (System.currentTimeMillis() - start) + "ms");
 		
-		//primaries[4].printRatio();
 		return choice;
+	}
+
+	private float[] propagate(float[] perception, Action action, float[] predSec) {
+		float[] new_context = new float[perception.length];
+		Arrays.fill(new_context, 0);
+		
+		int id_action = 0;
+		for (int i=0; i<Action.values().length; i++) {
+			if (Action.values()[i].equals(action)) {
+				id_action = i;
+				break;
+			}
+		}
+		
+		int action_offset = secondaries.length / Action.values().length;
+		for (int i=0; i < action_offset; i++) {
+			new_context[id_action*action_offset + i] = predSec[id_action*action_offset + i] > certitude_treshold? 1: 0;
+		}
+		new_context[predSec.length + id_action] = 1; 
+		
+		return new_context;
+	}
+
+	private DataStruct choose_function(DataStruct[] choice_list) {
+		DataStruct best = null;
+		boolean under_treshold = false;
+		float certainty = 1;
+		int utility = Integer.MIN_VALUE;
+		
+		for(DataStruct data: choice_list) {
+			if (under_treshold) {
+				if (Math.abs(data.certitude) < certainty) {
+					certainty = Math.abs(data.certitude);
+					best = data;
+				}
+			} else if (Math.abs(data.certitude) < certitude_treshold) {
+				best = data;
+				certainty = Math.abs(data.certitude);
+				under_treshold = true;
+			} else if (data.utility > utility) {
+				best = data;
+				utility = data.utility;
+			}
+		}
+		return best;
+	}
+
+	private DataStruct getValue(Action first_action, float[] context, int depth, int current_utility) {
+		Vector<Action> enactables = new Vector<Action>();
+		
+		// 1) checking if uncertain interesting prediction
+		// 1.1) making primary and secondary predictions
+		Vector<Float> predPrim = new Vector<Float>(primaries.length);
+		for (SelectiveNeuron neuron: primaries) {
+			predPrim.add(neuron.compute(context));
+		}
+		float[] predSec = getPredSec(context);
+		
+		// 1.2) isolate enactable interactions
+		HashMap<Integer, Float> enactable = new HashMap<Integer, Float>();
+		for (int index=0; index<predPrim.size(); index++) {			
+			enactable.put(index + secondaries.length, predPrim.get(index));
+			if (predPrim.get(index) >= certitude_treshold) enactables.add(Action.values()[index]);
+		}
+		for (int index=0; index<predSec.length; index++) {			
+			if (predPrim.get((int) (Math.floor(index / (secondaries.length / Action.values().length)))) >= certitude_treshold 
+					&& SelectiveNeuron.isInteresting(secondaries[index], Math.signum(predSec[index]))) {
+				enactable.put(index, predSec[index]);
+			}
+		}
+		
+		DataStruct[] dataset = new DataStruct[enactables.size()];
+
+		// 1.3) find the most uncertain prediction
+		float min_abs = 1;
+		float min = 0;
+		for (int key: enactable.keySet()) {
+			if (Math.abs(enactable.get(key)) < min_abs) {
+				min_abs = Math.abs(enactable.get(key));
+				min = enactable.get(key);
+			}
+		}
+		
+		// 1.4) if under treshold, return it.
+		if (min_abs < certitude_treshold) {
+			return new DataStruct(first_action, min, 0);
+		}
+		// 2) else if not at max depth, look further
+		else if (depth < Research_depth) {
+			for (int i=0; i<enactables.size(); i++) {
+				dataset[i] = getValue(first_action, 
+						propagate(context, enactables.get(i), predSec), 
+						depth+1, 
+						current_utility + utilities.get(enactables.get(i)));
+			}
+		}
+		// 3) else return action with highest utility
+		else {
+			for (int i=0; i<enactables.size(); i++) {
+				dataset[i] = new DataStruct(first_action, 1, current_utility + utilities.get(enactables.get(i)));
+			}
+		}
+		
+		return choose_function(dataset);
+	}
+
+	private float[] getPredSec(float[] perception) {
+		float[] res = new float[secondaries.length];
+		
+		// get predictions of success.... but EVEN FASTER
+				ExecutorService ex = Executors.newFixedThreadPool(Thread_nb);
+				int block_size = secondaries.length/Thread_nb;
+				for (int i = 0; i < Thread_nb-1; i++) {
+					final int istart = i * block_size;
+					final int iend = (i + 1) * block_size;
+					ex.execute(new Runnable() {
+						@Override
+						public void run() {
+							for (int j=istart; j<iend; j++) {
+								res[j] = secondaries[j].compute(perception);
+							}
+						}
+					});
+				}
+				ex.execute(new Runnable() {
+					@Override
+					public void run() {
+						for (int j=(Thread_nb-1)*block_size; j<secondaries.length; j++) {
+							res[j] = secondaries[j].compute(perception);
+						}
+					}
+				});
+
+				ex.shutdown();
+				try {
+					ex.awaitTermination(1, TimeUnit.MINUTES);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+		
+		return res;
 	}
 
 	private Action mostUseful(Vector<Float> predPrimaries) {
@@ -318,6 +463,14 @@ public class AgentDeveloppemental extends Agent{
 	public void updateNeurons() {
 		for (SelectiveNeuron n: primaries) n.update();
 		for (SelectiveNeuron n: secondaries) n.update();
+	}
+	
+	public int correspondingInteraction(int index) {
+		if (index < data_size - Action.values().length) {
+			return (int) Math.floor(index / (data_size - Action.values().length)/Action.values().length) ;
+		} else {
+			return (int) (index - (data_size - Action.values().length));
+		}
 	}
 
 }
